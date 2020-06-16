a04323fd140f5ac41b8b760ceaaca2fe093b682e
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-06-16 11:03:32 +0200

    next typescript prisma apollo setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import useSWR from 'swr'
+import { PrismaClient, Song, Artist } from '@prisma/client';
 
 const fetcher = (query) =>
   fetch('/api/graphql', {
@@ -11,19 +12,37 @@ const fetcher = (query) =>
     .then((res) => res.json())
     .then((json) => json.data)
 
-export default function Index() {
+export default function Index({ songs }: {songs: Song[]) {
   const { data, error } = useSWR('{ users { name } }', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
 
   const { users } = data
-
+  console.log(songs)
   return (
     <div>
       {users.map((user, i) => (
         <div key={i}>{user.name}</div>
       ))}
+      <ul>
+        {songs.map((song) => (
+          <li key={song.id}>{song.name}</li>
+        ))}
+      </ul>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const songs = await prisma.song.findMany({
+    include: { artist: true },
+  })
+
+  return {
+    props: {
+      songs,
+    },
+  }
 }
