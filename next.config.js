23a796da3685ad72d4504fba57c6c28dc6ce8142
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-07-13 12:32:22 +0200

    add bundle analyzer

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
