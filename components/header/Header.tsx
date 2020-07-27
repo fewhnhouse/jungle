d52bb2e5717305fdf20fe3b5f09ebf34f6135a41
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-07-27 14:26:03 +0200

    adjust header and login page

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,9 +4,10 @@ import { useScrollPosition } from '../../util/useScrollPosition'
 import Notifications from './Notifications'
 import Profile from './Profile'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import useMedia from 'use-media'
 import { Input } from 'rsuite'
+import { useSpring, animated } from 'react-spring'
 
 interface HeaderProps {
     landing: boolean
@@ -15,18 +16,26 @@ interface HeaderProps {
 const StyledHeader = styled.header<HeaderProps>`
     height: 60px;
     padding: 0px 10px;
-    width: 100%;
+    border-radius: 8px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    position: fixed;
-    top: 0px;
     background-color: white;
     z-index: 100;
-    box-shadow: ${({ scrolled }) =>
-        scrolled ? 'rgba(0, 0, 0, 0.5) 0px 0px 15px 0px' : ''};
+    margin: 20px auto;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
     transition: box-shadow 0.3s ease-in-out;
+    min-width: 400px;
+    width: 90%;
+    max-width: 1000px;
+`
+
+const HeaderContainer = styled.div`
+    height: 100px;
+    width: 100%;
+    position: fixed;
+    top: 0px;
 `
 
 const StyledLink = styled.a`
@@ -45,21 +54,24 @@ const Options = styled.div`
 `
 
 const InputContainer = styled.div`
-    position: absolute;
-    right: 120px;
-    top: 8px;
+    position: relative;
+    right: 20px;
+    top: 0px;
     transition: width 0.2s ease-in-out, height 0.2s ease-in-out,
         box-shadow 0.2s ease-in-out;
-    width: ${({ opened }: { opened: boolean }) =>
-        opened ? window.innerWidth - 240 + 'px' : '200px'};
-    height: ${({ opened }: { opened: boolean }) => (opened ? '300px' : '48px')};
+    width: ${({
+        opened,
+        headerWidth,
+    }: {
+        opened: boolean
+        headerWidth?: number
+    }) => (opened ? (headerWidth ?? 400) / 2 + 'px' : '200px')};
     background: white;
     border-radius: 6px;
     box-shadow: ${({ opened }: { opened: boolean }) =>
         opened ? 'rgba(0,0,0,0.5) 0px 0px 15px 0px' : ''};
     padding: ${({ theme }) => theme.spacing.mini};
 `
-
 
 const WrappedLink = ({
     children,
@@ -71,6 +83,8 @@ const WrappedLink = ({
 )
 
 const Header = () => {
+    const ref = useRef()
+    console.log(ref.current)
     const router = useRouter()
     const isMobile = useMedia('screen and (max-width: 400px)')
     const { pathname, query } = router
@@ -82,54 +96,66 @@ const Header = () => {
     const onBlur = () => setOpen(false)
 
     const { y } = useScrollPosition()
+
     return (
         !isMobile && (
-            <StyledHeader landing={pathname === '/'} scrolled={y > 0}>
-                {pathname.includes('/projects/') ? (
-                    <Links>
-                        <WrappedLink href="/">Home</WrappedLink>
-                        <WrappedLink
-                            href="/projects/[id]/board"
-                            as={`/projects/${id}/board`}
-                        >
-                            Board
-                        </WrappedLink>
-                        <WrappedLink
-                            href="/projects/[id]/backlog"
-                            as={`/projects/${id}/backlog`}
-                        >
-                            Backlog
-                        </WrappedLink>
-                        <WrappedLink
-                            href="/projects/[id]/reports"
-                            as={`/projects/${id}/reports`}
-                        >
-                            Reports
-                        </WrappedLink>
-                    </Links>
-                ) : pathname === '/login' ? (
-                    <h3></h3>
-                ) : pathname.includes('/') ? (
-                    <Links>
-                        <WrappedLink href="/">Home</WrappedLink>
-                        <WrappedLink href="/projects">Projects</WrappedLink>
-                        <WrappedLink href="/activity">Activity</WrappedLink>
-                        <WrappedLink href="/your-work">Your Work</WrappedLink>
-                    </Links>
-                ) : null}
+            <HeaderContainer>
+                <StyledHeader
+                    ref={ref}
+                    landing={pathname === '/'}
+                    scrolled={y > 0}
+                >
+                    {pathname.includes('/projects/') ? (
+                        <Links>
+                            <WrappedLink href="/">Home</WrappedLink>
+                            <WrappedLink
+                                href="/projects/[id]/board"
+                                as={`/projects/${id}/board`}
+                            >
+                                Board
+                            </WrappedLink>
+                            <WrappedLink
+                                href="/projects/[id]/backlog"
+                                as={`/projects/${id}/backlog`}
+                            >
+                                Backlog
+                            </WrappedLink>
+                            <WrappedLink
+                                href="/projects/[id]/reports"
+                                as={`/projects/${id}/reports`}
+                            >
+                                Reports
+                            </WrappedLink>
+                        </Links>
+                    ) : pathname === '/login' ? (
+                        <h3></h3>
+                    ) : pathname.includes('/') ? (
+                        <Links>
+                            <WrappedLink href="/">Home</WrappedLink>
+                            <WrappedLink href="/projects">Projects</WrappedLink>
+                            <WrappedLink href="/activity">Activity</WrappedLink>
+                            <WrappedLink href="/your-work">
+                                Your Work
+                            </WrappedLink>
+                        </Links>
+                    ) : null}
 
-                <Options>
-                    <InputContainer opened={open}>
-                        <Input
-                            onFocus={onFocus}
-                            onBlur={onBlur}
-                            placeholder="Search..."
-                        />
-                    </InputContainer>
-                    <Notifications />
-                    <Profile />
-                </Options>
-            </StyledHeader>
+                    <Options>
+                        <InputContainer
+                            headerWidth={ref?.current?.clientWidth}
+                            opened={open}
+                        >
+                            <Input
+                                onFocus={onFocus}
+                                onBlur={onBlur}
+                                placeholder="Search..."
+                            />
+                        </InputContainer>
+                        <Notifications />
+                        <Profile />
+                    </Options>
+                </StyledHeader>
+            </HeaderContainer>
         )
     )
 }
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,18 +1,21 @@
-import {
-    Card,
-    CardHeader,
-    FormInput,
-    CardBody,
-    CardFooter,
-    Button,
-    FormCheckbox,
-} from 'shards-react'
 import styled from 'styled-components'
 import Link from 'next/link'
 import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { IUser } from '../../interfaces/User'
+import {
+    Panel,
+    Button,
+    Checkbox,
+    Form,
+    FormGroup,
+    ButtonToolbar,
+    ControlLabel,
+    FormControl,
+    Alert,
+    CheckboxGroup,
+} from 'rsuite'
 
 const Container = styled.div`
     display: flex;
@@ -24,94 +27,85 @@ const Container = styled.div`
     align-items: center;
 `
 
-const StyledCard = styled(Card)`
-    margin: 10px;
-    width: 300px;
-`
-
-const StyledBody = styled(CardBody)`
-    padding: 32px 16px;
-`
-
-const StyledFooter = styled(CardFooter)`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 16px;
-`
-
-const StyledFormInput = styled(FormInput)`
-    margin: 10px 0px;
-`
-
-const StyledButton = styled(Button)`
-    margin: 0px 5px;
+const StyledPanel = styled(Panel)`
+    background: white;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
 `
 
 export default function Home() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [formState, setFormState] = useState({
+        username: '',
+        password: '',
+        remember: '',
+    })
+
     const { push } = useRouter()
 
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-        setPassword(e.target.value)
+    const handleLogin = async () => {
+        const { username, password, remember } = formState
+        try {
+            const { data } = await axios.post<unknown, { data: IUser }>(
+                '/auth',
+                {
+                    username,
+                    password,
+                    type: 'normal',
+                }
+            )
 
-    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-        setUsername(e.target.value)
-
-    const handleLogin = async (e: React.FormEvent) => {
-        e.preventDefault()
-        const { data } = await axios.post<unknown, { data: IUser }>('/auth', {
-            username,
-            password,
-            type: 'normal',
-        })
-
-        const { id, auth_token, username: name, email } = data
-        localStorage.setItem(
-            'user',
-            JSON.stringify({ id, username: name, email })
-        )
-        localStorage.setItem('auth-token', auth_token)
-        push('/')
+            const { id, auth_token, username: name, email } = data
+            localStorage.setItem(
+                'user',
+                JSON.stringify({ id, username: name, email })
+            )
+            localStorage.setItem('auth-token', auth_token)
+            push('/')
+        } catch (e) {
+            Alert.error('Login failed.')
+        }
     }
     return (
         <>
             <Container>
-                <form onSubmit={handleLogin}>
-                    <StyledCard>
-                        <CardHeader>
-                            <h3>Into the Jungle</h3>
-                        </CardHeader>
-                        <StyledBody>
-                            <StyledFormInput
-                                onChange={handleUsernameChange}
-                                value={username}
-                                placeholder="username"
-                                type="text"
-                            />
-                            <StyledFormInput
-                                onChange={handlePasswordChange}
-                                value={password}
-                                placeholder="password"
-                                type="password"
-                            />
-                            <p>
-                                Forgot your password? Reset it{' '}
-                                <Link href="reset-password">
-                                    <a>here</a>
-                                </Link>
-                                .
-                            </p>
-                        </StyledBody>
-                        <StyledFooter>
-                            <FormCheckbox>Remember Me</FormCheckbox>
-                            <StyledButton type="submit" theme="success">
-                                Log In &rarr;
-                            </StyledButton>
-                        </StyledFooter>
-                    </StyledCard>
-                </form>
+                <StyledPanel bordered title={<h3>Into the Jungle</h3>}>
+                    <Form
+                        onSubmit={handleLogin}
+                        value={formState}
+                        onChange={(value) => setFormState(value)}
+                    >
+                        <FormGroup>
+                            <ControlLabel>Username</ControlLabel>
+                            <FormControl name="username" />
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel>Password</ControlLabel>
+                            <FormControl name="password" type="password" />
+                        </FormGroup>
+                        <p>
+                            Forgot your password? Reset it{' '}
+                            <Link href="reset-password">
+                                <a>here</a>
+                            </Link>
+                            .
+                        </p>
+                        <FormGroup>
+                            <FormControl
+                                accepter={CheckboxGroup}
+                                name="remember"
+                                inline
+                            >
+                                <Checkbox>Remember me</Checkbox>
+                            </FormControl>
+                        </FormGroup>
+                        <FormGroup>
+                            <ButtonToolbar>
+                                <Button type="submit" appearance="primary">
+                                    Log In &rarr;
+                                </Button>
+                            </ButtonToolbar>
+                        </FormGroup>
+                    </Form>
+                </StyledPanel>
             </Container>
         </>
     )
