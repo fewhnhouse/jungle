f7ebe3c2a079b0a70537c0e44443f9c6697408b6
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-09-18 13:36:54 +0200

    project detail settings.

---
+++ components/project-settings/ProjectSettings.tsx
@@ -0,0 +1,173 @@
+import { useRouter } from 'next/router'
+import {
+    Form,
+    Input,
+    Panel,
+    FormGroup,
+    FormControl,
+    Button,
+    Checkbox,
+} from 'rsuite'
+import styled from 'styled-components'
+import Flex from '../Flex'
+
+const StyledPanel = styled(Panel)`
+    margin: 30px 0px;
+    &:first-child {
+        margin-top: 0px;
+    }
+    padding: 0px;
+    max-width: 500px;
+`
+
+const Avatar = styled.img`
+    height: 100%;
+    width: 100%;
+    transition: all 0.3s ease;
+    object-fit: contain;
+`
+
+const AvatarWrapper = styled.div`
+    position: relative;
+    height: 100px;
+    width: 100px;
+    margin: 10px auto;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 1px 1px 15px -5px black;
+    transition: all 0.3s ease;
+    cursor: pointer;
+    &:hover {
+        transform: scale(1.05);
+    }
+
+    &:hover ${Avatar} {
+        opacity: 0.5;
+    }
+`
+
+const FileInput = styled.input`
+    width: 0;
+    height: 0;
+`
+
+const UploadButton = styled.label`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+`
+
+const Footer = styled.div`
+    border-top: 1px solid #e5e5ea;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    min-height: 60px;
+    background: #fafafa;
+`
+
+const StyledFormGroup = styled(FormGroup)`
+    padding: 10px 20px;
+`
+
+const ProjectSettings = () => {
+    const { projectId } = useRouter().query
+
+    return (
+        <div>
+            <StyledPanel bodyFill bordered header="Project Name">
+                <Form>
+                    <StyledFormGroup>
+                        <FormControl name="name" />
+                    </StyledFormGroup>
+                    <Footer>
+                        <span>Your Project name is visible to everyone.</span>
+                        <Button appearance="ghost">Save</Button>
+                    </Footer>
+                </Form>
+            </StyledPanel>
+            <StyledPanel bodyFill bordered header="Project Description">
+                <Form>
+                    <StyledFormGroup>
+                        <FormControl
+                            rows={5}
+                            name="textarea"
+                            componentClass="textarea"
+                        />
+                    </StyledFormGroup>
+                    <Footer>
+                        <span>
+                            Your Project description makes others understand
+                            what this project is about.
+                        </span>
+                        <Button appearance="ghost">Save</Button>
+                    </Footer>
+                </Form>
+            </StyledPanel>
+            <StyledPanel bodyFill bordered header="Avatar">
+                <Form>
+                    <StyledFormGroup>
+                        <Flex align="center" justify="space-between">
+                            <span>
+                                Click the icon to change your Avatar.
+                                <br /> It is optional, but strongly recommended.
+                            </span>
+                            <AvatarWrapper>
+                                <Avatar src="https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png" />
+                                <UploadButton htmlFor="avatar-upload"></UploadButton>
+                                <FileInput
+                                    id="avatar-upload"
+                                    className="file-upload"
+                                    type="file"
+                                    accept="image/*"
+                                />
+                            </AvatarWrapper>
+                        </Flex>
+                    </StyledFormGroup>
+
+                    <Footer>
+                        <span>
+                            Your Avatar helps other people recognize you.
+                        </span>
+                    </Footer>
+                </Form>
+            </StyledPanel>
+            <StyledPanel bodyFill bordered header="Visibility">
+                <Form>
+                    <StyledFormGroup>
+                        <span>
+                            Your Project is currently is visible to everyone.
+                        </span>
+                    </StyledFormGroup>
+                    <Footer>
+                        <span>Your Project is currently set to public.</span>
+                        <Button appearance="ghost">Take Private</Button>
+                    </Footer>
+                </Form>
+            </StyledPanel>
+            <StyledPanel bodyFill bordered header="Delete Project">
+                <Form>
+                    <StyledFormGroup>
+                        <span>
+                            If you delete your project, you wont be able to
+                            restore it later.
+                        </span>
+                    </StyledFormGroup>
+                    <Footer>
+                        <Checkbox>
+                            Confirm that you want to delete your project.
+                        </Checkbox>
+                        <Button color="red" appearance="ghost">
+                            Delete Project
+                        </Button>
+                    </Footer>
+                </Form>
+            </StyledPanel>
+        </div>
+    )
+}
+
+export default ProjectSettings
--- pages/projects/[projectId]/settings.tsx
+++ pages/projects/[projectId]/settings.tsx
@@ -1,20 +1,46 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 import styled from 'styled-components'
 import Flex from '../../../components/Flex'
 import { PageBody, PageHeader } from '../../../components/Layout'
 import PageTitle from '../../../components/PageTitle'
+import ProjectSettings from '../../../components/project-settings/ProjectSettings'
 import { getProject } from '../../../taiga-api/projects'
 
 const Menu = styled.nav`
     display: flex;
     flex-direction: column;
+    margin: 20px 0px;
+    width: 300px;
 `
 
-const Content = styled.div``
+const MenuItem = styled.button<{ active?: boolean }>`
+    border: none;
+    text-align: left;
+    background: none;
+    color: #555;
+    font-weight: ${({ active }) => (active ? 'bold' : 'normal')};
+    padding: 10px;
+    &:hover {
+        color: #333;
+    }
+`
+
+const Content = styled.div`
+    margin: 20px 20px;
+    margin-left: 40px;
+    min-width: 500px;
+`
+
+const StyledFlex = styled(Flex)`
+    width: min-content;
+    margin: auto;
+`
 
 export default function Settings() {
     const { projectId } = useRouter().query
+    const [tabIndex, setTabIndex] = useState(0)
 
     const { data: project } = useQuery(
         ['project', { projectId }],
@@ -22,6 +48,17 @@ export default function Settings() {
         { enabled: projectId }
     )
 
+    const handleTabClick = (index: number) => () => setTabIndex(index)
+
+    const tabs = [
+        'Project Details',
+        'Modules',
+        'Default Values',
+        'Attributes',
+        'Members',
+        'Permissions',
+        'Integrations',
+    ]
     return (
         <div>
             <PageHeader>
@@ -31,10 +68,20 @@ export default function Settings() {
                 />
             </PageHeader>
             <PageBody>
-                <Flex>
-                    <Menu>nav</Menu>
-                    <Content>content</Content>
-                </Flex>
+                <StyledFlex justify="flex-start">
+                    <Menu>
+                        {tabs.map((tab, index) => (
+                            <MenuItem
+                                onClick={handleTabClick(index)}
+                                key={index}
+                                active={index === tabIndex}
+                            >
+                                {tab}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                    <Content>{tabIndex === 0 && <ProjectSettings />}</Content>
+                </StyledFlex>
             </PageBody>
         </div>
     )
