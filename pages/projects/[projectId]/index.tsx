d89326d06c436a89371ec5c1c235e84896e35e34
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-09-25 14:20:29 +0200

    project overview page.

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { PageBody, PageHeader } from '../../../components/Layout'
+import PageTitle from '../../../components/PageTitle'
+import { useQuery } from 'react-query'
+import { getProject } from '../../../taiga-api/projects'
 
 const Container = styled.div`
     display: flex;
@@ -13,15 +17,34 @@ const Project = () => {
     const router = useRouter()
     const { projectId } = router.query
 
+    const { data } = useQuery(
+        ['project', { projectId }],
+        (key, { projectId }) => getProject(projectId as string)
+    )
+
     return (
-        <Container>
-            <Link href="/projects/[id]/board" as={`/projects/${projectId}/board`}>
-                <a>Board</a>
-            </Link>
-            <Link href="/projects/[id]/backlog" as={`/projects/${projectId}/backlog`}>
-                <a>Backlog</a>
-            </Link>
-        </Container>
+        <>
+            <PageHeader>
+                <PageTitle
+                    title={data?.name}
+                    description={data?.description}
+                />
+            </PageHeader>
+            <PageBody>
+                <Link
+                    href="/projects/[id]/board"
+                    as={`/projects/${projectId}/board`}
+                >
+                    <a>Board</a>
+                </Link>
+                <Link
+                    href="/projects/[id]/backlog"
+                    as={`/projects/${projectId}/backlog`}
+                >
+                    <a>Backlog</a>
+                </Link>
+            </PageBody>
+        </>
     )
 }
 
