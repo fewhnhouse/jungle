1fb153d3cd90c81864ebce6fb71ecff6c8ff5c1d
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-12-14 14:57:50 +0100

    settings disabled for non-members

--- pages/projects/[projectId]/settings.tsx
+++ pages/projects/[projectId]/settings.tsx
@@ -14,9 +14,10 @@ import Members from '../../../components/project-settings/members'
 import Points from '../../../components/project-settings/points'
 import Tags from '../../../components/project-settings/tags'
 import useMedia from 'use-media'
-import { Result } from 'antd'
+import { Button, Result, Skeleton } from 'antd'
 import Flex from '../../../components/Flex'
 import CustomFields from '../../../components/project-settings/customFields'
+import { getMe } from '../../../taiga-api/users'
 
 const HeaderContainer = styled.div`
     margin: auto;
@@ -24,14 +25,16 @@ const HeaderContainer = styled.div`
 `
 
 export default function ProjectSettings() {
-    const { projectId } = useRouter().query
+    const { back, query } = useRouter()
+    const { projectId } = query
     const [menuItemIndex, setMenuItemIndex] = useState(0)
     const isMobile = useMedia('(max-width: 900px)')
-    const { data: project } = useQuery(
+    const { data: project, isLoading: isProjectLoading } = useQuery(
         ['project', { projectId }],
         (key, { projectId }) => getProject(projectId as string),
         { enabled: projectId }
     )
+    const { data: me, isLoading: isMeLoading } = useQuery('me', () => getMe())
 
     const handleItemClick = (index: number) => () => setMenuItemIndex(index)
 
@@ -46,6 +49,10 @@ export default function ProjectSettings() {
         // 'Permissions',
         // 'Integrations',
     ]
+
+    const isLoading = isMeLoading || isProjectLoading
+
+    const hasAccess = project?.members.find((m) => m.id === me?.id)
 
     return (
         <div>
@@ -66,28 +73,50 @@ export default function ProjectSettings() {
                 </HeaderContainer>
             </PageHeader>
             <PageBody>
-                {isMobile ? (
-                    <Flex fluid align="center" justify="center">
-                        <Result
-                            title="Settings are not supported on small screens yet."
-                            status="warning"
-                        />
-                    </Flex>
-                ) : (
-                    <Settings
-                        onMenuItemClick={handleItemClick}
-                        menuItems={menuItems}
-                        menuIndex={menuItemIndex}
-                    >
-                        {menuItemIndex === 0 && <ProjectDetails />}
-                        {menuItemIndex === 1 && <DefaultValues />}
-                        {menuItemIndex === 2 && <Statuses />}
-                        {menuItemIndex === 3 && <Points />}
-                        {menuItemIndex === 4 && <CustomFields />}
-                        {menuItemIndex === 5 && <Tags />}
-                        {menuItemIndex === 6 && <Members />}
-                    </Settings>
-                )}
+                <Skeleton loading={isLoading}>
+                    {hasAccess ? (
+                        <>
+                            {isMobile ? (
+                                <Flex fluid align="center" justify="center">
+                                    <Result
+                                        title="Settings are not supported on small screens yet."
+                                        status="warning"
+                                    />
+                                </Flex>
+                            ) : (
+                                <Settings
+                                    onMenuItemClick={handleItemClick}
+                                    menuItems={menuItems}
+                                    menuIndex={menuItemIndex}
+                                >
+                                    {menuItemIndex === 0 && <ProjectDetails />}
+                                    {menuItemIndex === 1 && <DefaultValues />}
+                                    {menuItemIndex === 2 && <Statuses />}
+                                    {menuItemIndex === 3 && <Points />}
+                                    {menuItemIndex === 4 && <CustomFields />}
+                                    {menuItemIndex === 5 && <Tags />}
+                                    {menuItemIndex === 6 && <Members />}
+                                </Settings>
+                            )}
+                        </>
+                    ) : (
+                        <Flex
+                            fluid
+                            justify="center"
+                            align="center"
+                        >
+                            <Result
+                                title="You do not have access to view this page."
+                                status="403"
+                                extra={
+                                    <Button onClick={back} type="primary">
+                                        Go Back
+                                    </Button>
+                                }
+                            />
+                        </Flex>
+                    )}
+                </Skeleton>
             </PageBody>
         </div>
     )
